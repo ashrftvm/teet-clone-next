e2b0724f1c5ce1eb33332f115c2e305c9e204674
Ashrf <ashrftvm1@gmail.com>
2023-07-11 01:27:39 +0400

    completed fetching user image and signout

---
+++ app/auth/Logged.tsx
@@ -0,0 +1,19 @@
+'use client'
+import { signOut } from "next-auth/react"
+import Link from "next/link"
+import Image from "next/image"
+
+type User = {
+    image: string
+}
+
+export default function Logged({image}:User){
+    return(
+        <li className="flex gap-8 items-center">
+            <button onClick={()=>signOut()}>Sign Out</button>
+            <Link href={"/dashboard"}>
+                <Image height={64} width={64} src={image} alt="Google account image" className="w-14 rounded-full"></Image>
+            </Link>
+        </li>
+    )
+}
