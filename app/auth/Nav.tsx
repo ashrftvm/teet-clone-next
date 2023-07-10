85e5f5f287f58647bc14c7f38481ec35f67b62e1
Ashrf <ashrftvm1@gmail.com>
2023-07-10 23:50:04 +0400

    auth component with login from next-auth

--- app/auth/Nav.tsx
+++ app/auth/Nav.tsx
@@ -1,9 +1,15 @@
 import Link from "next/link"
+import Login from "./Login"
 
-export default async function (){
+export default async function () {
     return (
-        <Link href={"/"}>
-            <h1>Send it.</h1>
-        </Link>
+        <nav className="flex justify-between items-center py-8">
+            <Link href={"/"}>
+                <h1 className="font-bold text-lg">Send it.</h1>
+            </Link>
+            <ul className="flex items-center gap-6">
+                <Login></Login>
+            </ul>
+        </nav>
     )
 }
